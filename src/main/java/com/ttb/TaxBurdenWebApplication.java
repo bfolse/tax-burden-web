80beebcc9fc3aa50bb104262c101e3d22a0f4ead
Brian Folse <bmfolse@gmail.com>
2018-10-25 12:13:35 -0400

    AWS

--- src/main/java/com/ttb/TaxBurdenWebApplication.java
+++ src/main/java/com/ttb/TaxBurdenWebApplication.java
@@ -1,9 +1,14 @@
 package com.ttb;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +17,14 @@ public class TaxBurdenWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaxBurdenWebApplication.class, args);
+	}
+
+	@Bean
+	@Profile("awsdev")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		b.setDataCenterInfo(info);
+		return b;
 	}
 }
