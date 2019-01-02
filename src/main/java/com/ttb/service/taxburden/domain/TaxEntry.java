19cd2ff0d3ae254388f7723ee48d125aa8f59f76
Brian Folse <bmfolse@gmail.com>
2019-01-02 13:16:33 -0500

    add description to tax entry

--- src/main/java/com/ttb/service/taxburden/domain/TaxEntry.java
+++ src/main/java/com/ttb/service/taxburden/domain/TaxEntry.java
@@ -1,9 +1,12 @@
 package com.ttb.service.taxburden.domain;
 
 
+import java.util.Objects;
+
 public class TaxEntry {
 	private TaxType taxType;
 	private PoliticalDivision politicalDivision;
+	private String description;
 	private MonetaryAmount amount;
 
 	/**
@@ -18,10 +21,11 @@ public class TaxEntry {
 	 * @param politicalDivision
 	 * @param amount
 	 */
-	public TaxEntry(TaxType taxType, PoliticalDivision politicalDivision, MonetaryAmount amount) {
+	public TaxEntry(TaxType taxType, PoliticalDivision politicalDivision, String description, MonetaryAmount amount) {
 		super();
 		this.taxType = taxType;
 		this.politicalDivision = politicalDivision;
+		this.description = description;
 		this.amount = amount;
 	}
 
@@ -67,52 +71,37 @@ public class TaxEntry {
 		this.amount = amount;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof TaxEntry)) return false;
+		TaxEntry taxEntry = (TaxEntry) o;
+		return taxType == taxEntry.taxType &&
+				Objects.equals(politicalDivision, taxEntry.politicalDivision) &&
+				Objects.equals(description, taxEntry.description) &&
+				Objects.equals(amount, taxEntry.amount);
+	}
+
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
-		result = prime * result + ((politicalDivision == null) ? 0 : politicalDivision.hashCode());
-		result = prime * result + ((taxType == null) ? 0 : taxType.hashCode());
-		return result;
+		return Objects.hash(taxType, politicalDivision, description, amount);
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TaxEntry other = (TaxEntry) obj;
-		if (amount == null) {
-			if (other.amount != null)
-				return false;
-		} else if (!amount.equals(other.amount))
-			return false;
-		if (politicalDivision == null) {
-			if (other.politicalDivision != null)
-				return false;
-		} else if (!politicalDivision.equals(other.politicalDivision))
-			return false;
-		if (taxType != other.taxType)
-			return false;
-		return true;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
-		return "TaxEntry [taxType=" + taxType + ", politicalDivision=" + politicalDivision + ", amount=" + amount + "]";
+		return "TaxEntry{" +
+				"taxType=" + taxType +
+				", politicalDivision=" + politicalDivision +
+				", description='" + description + '\'' +
+				", amount=" + amount +
+				'}';
 	}
-
 }
